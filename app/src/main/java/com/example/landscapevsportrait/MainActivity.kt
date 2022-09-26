489d7116d4213387990a06ad33423606614b484b
yagsim <yagosimofernandez@gmail.com>
2022-09-26 09:03:05 +0200

    Programa cambio según modo de vista

---
+++ app/src/main/java/com/example/landscapevsportrait/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.landscapevsportrait
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var texto:TextView=findViewById(R.id.text)
+
+
+    }
+}
